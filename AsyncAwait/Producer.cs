74f42a0571040b26e4b740b76e81423d97a990ff
Diego Bonura <diego@bonura.dev>
2023-10-07 18:44:15 +0200

    Add few async examples

--- AsyncAwait/Producer.cs
+++ AsyncAwait/Producer.cs
@@ -13,6 +13,27 @@ namespace AsyncAwait
             {
                 var result = await consumer.ElaborateAsync(i, i);
                 Console.WriteLine($"Counter: {i} with result: {result}");
+
+                // Two task
+                //var result1 = await consumer.ElaborateAsync(i, i);
+                //Console.WriteLine($"Counter1: {i} with result: {result1}");
+                //var result2 = await consumer.ElaborateAsync(i, i);
+                //Console.WriteLine($"Counter2: {i} with result: {result2}");
+
+                // A chain of tasks
+                //var result = await (consumer.ElaborateAsync(i, i).ContinueWith(async (result1) =>
+                //{
+                //    Console.WriteLine($"Counter1: {i} with result: {result1.Result}");
+                //    return await consumer.ElaborateAsync(i, i);
+                //}));
+                //Console.WriteLine($"Counter2: {i} with result: {result.Result}");
+
+                // Two task in parallel
+                //var taskResult1 = consumer.ElaborateAsync(i, i);
+                //var taskResult2 = consumer.ElaborateAsync(i, i);
+                //Task.WaitAll(new Task[] { taskResult1, taskResult2 });
+                //Console.WriteLine($"Counter1: {i} with result: {taskResult1.Result}");
+                //Console.WriteLine($"Counter2: {i} with result: {taskResult2.Result}");
                 i++;
             }
         }
