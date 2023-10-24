32af897375d9a0f5bd62c5be946092993773e3da
Diego Bonura <diego@bonura.dev>
2023-10-24 21:13:40 +0200

    Add distributed lock example

---
+++ DistributedLockWithRedisA/Program.cs
@@ -0,0 +1,27 @@
+﻿using RedLockNet.SERedis;
+using RedLockNet.SERedis.Configuration;
+using System.Net;
+
+namespace DistributedLockWithRedisA
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var endPoints = new List<RedLockEndPoint> { new DnsEndPoint("localhost", 6379) };
+            var redlockFactory = RedLockFactory.Create(endPoints);
+
+            var resource = "my-order-id";
+            var expiry = TimeSpan.FromSeconds(30);
+
+            await using (var redLock = await redlockFactory.CreateLockAsync(resource, expiry)) // there are also non async Create() methods
+            {
+                // make sure we got the lock
+                if (redLock.IsAcquired)
+                {
+                    // do stuff
+                }
+            }
+        }
+    }
+}
---
+++ DistributedLockWithRedisB/Program.cs
@@ -0,0 +1,27 @@
+﻿using RedLockNet.SERedis;
+using RedLockNet.SERedis.Configuration;
+using System.Net;
+
+namespace DistributedLockWithRedisB
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var endPoints = new List<RedLockEndPoint> { new DnsEndPoint("localhost", 6379) };
+            var redlockFactory = RedLockFactory.Create(endPoints);
+
+            var resource = "my-order-id";
+            var expiry = TimeSpan.FromSeconds(30);
+
+            await using (var redLock = await redlockFactory.CreateLockAsync(resource, expiry))
+            {
+                // make sure we got the lock
+                if (redLock.IsAcquired)
+                {
+                    // do stuff
+                }
+            }
+        }
+    }
+}
