e18a7fd1396f16cde4d4c9b3b590b8ed90042ff3
Diego Bonura <diego@bonura.dev>
2023-11-18 12:00:47 +0100

    Add vault example

---
+++ SecretsWithVault/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.AuthMethods.Token;
+
+namespace SecretsWithVault
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Initialize one of the several auth methods.
+            IAuthMethodInfo authMethod = new TokenAuthMethodInfo("testtoken");
+
+            // Initialize settings. You can also set proxies, custom delegates etc. here.
+            var vaultClientSettings = new VaultClientSettings("http://localhost:8200", authMethod);
+
+            IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+
+            var myKeys = await vaultClient.V1.Secrets.Cubbyhole.ReadSecretAsync("my-path");
+        }
+    }
+}
